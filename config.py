cef258277b89a8b9c9017f36965aaeda73252f89
littleblack111 <littleblack11111@gmail.com>
2025-01-20 00:40:32 +0800

    init: config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+import configparser
+from keys import keys
+
+def config_groups():
+    config = configparser.ConfigParser()
+    config.read("config.ini")
+    if "keys" not in config:
+        config["keys"] = keys
+        with open("config.ini", "w") as configfile:
+            config.write(configfile)
+    return config
+
+def config_colors():
+    config = configparser.ConfigParser()
+    config.read("colors.ini")
+    if "colors" not in config:
+        config["colors"] = {
+            "groupname": "#00000"
+        }
+    if "groups" not in config:
+        config["groups"] = {
+            "groupname": "Esc, F1, F2, F3",
+            "groupname2": "F10 F11 F12"
+        }
+    with open("colors.ini", "w") as configfile:
+        config.write(configfile)
