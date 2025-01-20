f24b9af34b2d35364f6e8ff9b78f31d43f7bb539
littleblack111 <littleblack11111@gmail.com>
2025-01-20 18:04:20 +0800

    debug: logging

--- config.py
+++ config.py
@@ -1,27 +1,42 @@
 import configparser
 from keys import keys
+import logging
+
+logger = logging.getLogger(__name__)
 
 def config_groups() -> configparser.ConfigParser:
+    logger.debug("Parsing groups")
     config = configparser.ConfigParser()
+    logger.debug("Reading config.ini")
     config.read("config.ini")
     if "keys" not in config:
+        logger.warning("keys is not in config, adding default keys instead")
         config["keys"] = keys
+        logger.debug(f"keys defaults: {config['keys']}")
         with open("config.ini", "w") as configfile:
+            logger.debug("Writing config.ini")
             config.write(configfile)
     return config
 
 def config_colors() -> configparser.ConfigParser:
+    logger.debug("Parsing colors")
     config = configparser.ConfigParser()
+    logger.debug("Reading colors.ini")
     config.read("colors.ini")
     if "colors" not in config:
+        logger.warning("colors is not in config, adding default colors instead")
         config["colors"] = {
             "groupname": "#00000"
         }
+        logger.debug(f"colors defaults: {config['colors']}")
     if "groups" not in config:
+        logger.warning("groups is not in config, adding default groups instead")
         config["groups"] = {
             "groupname": "Esc, F1, F2, F3",
             "groupname2": "F10 F11 F12"
         }
+        logger.debug(f"groups defaults: {config['groups']}")
     with open("colors.ini", "w") as configfile:
+        logger.debug("Writing colors.ini")
         config.write(configfile)
     return config
--- fetch.py
+++ fetch.py
@@ -1,13 +1,19 @@
 from json import loads
 from requests import get
 from argparse import Namespace as ArgParsed, ArgumentParser
+import logging
+
+logger = logging.getLogger(__name__)
 
 def fetch_data(id: str) -> dict:
     if id.startswith("http"):
+        logger.debug(f"Fetching data from URL {id}")
         return loads(get(id).text)
+    logger.info(f"Fetching data for ID {id}")
     return loads(get(f"https://api.wooting.io/public/wootility/profiles?code={id}").text)
 
 def argparser() -> ArgParsed:
+    logger.debug("Parsing arguments")
     parser = ArgumentParser(prog="wootility-parser-fetcher", description="Parse Wooting profiles from Wootility Fetcher")
     parser.add_argument("id", help="The ID of the profile you want to fetch, obtain it by pressing share profile in Wootility", type=str)
     return parser.parse_args()
@@ -15,6 +21,7 @@ def argparser() -> ArgParsed:
 def main():
     args = argparser()
     data = fetch_data(args.id)
+    logger.debug(f"Fetched data: {data}")
     print(data)
 
 if __name__ == "__main__":
